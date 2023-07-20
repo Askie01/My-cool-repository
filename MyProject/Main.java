6943e2c7afe8d8e55010f0b44d6f7a4467ce072d
Askie01 <adam.paw01@gmail.com>
2023-07-21 00:10:36 +0200

    Added catSound method to the project

--- MyProject/Main.java
+++ MyProject/Main.java
@@ -3,4 +3,8 @@ public class Main {
         System.out.println("Hello world!");
 		System.out.println("This is my first project!");
     }
+	
+	public static void catSound() {
+        System.out.println("Meeeow!");
+    }
 }
