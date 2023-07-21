dd74a61dbd060122927d204a6cc5ef53a91d914b
Askie01 <adam.paw01@gmail.com>
2023-07-21 06:00:55 +0200

    fixed the problem

--- MyProject/Main.java
+++ MyProject/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-		System.out.println("This is my first project!");
+        System.out.println("This is my first project!");
+		System.out.println("Problem has been solved!");
     }
-	
-	public static void catSound() {
+
+    public static void catSound() {
         System.out.println("Meeeow!");
     }
 }
