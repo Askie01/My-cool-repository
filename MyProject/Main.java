df7c49cf9d97eb47f3a770b3163b431c1bdc7ced
Askie01 <adam.paw01@gmail.com>
2023-07-21 06:12:47 +0200

    Fixed the file

--- MyProject/Main.java
+++ MyProject/Main.java
@@ -1,15 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("This is my first project!");
 		System.out.println("Problem has been solved!");
+        System.out.println("This is my first project!");
     }
 
     public static void catSound() {
         System.out.println("Meeeow!");
-    }
-	
-	public static void dogSound() {
-        System.out.println("Woof woof!");
     }
 }
