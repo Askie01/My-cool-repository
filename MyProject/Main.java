f4a2a39411603a6b46b83001272844fff452076b
Askie01 <133720459+Askie01@users.noreply.github.com>
2023-07-24 17:05:18 +0200

    Update Main.java
    Added another line of code.

--- MyProject/Main.java
+++ MyProject/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-		System.out.println("Problem has been solved!");
+	System.out.println("Problem has been solved!");
         System.out.println("This is my first project!");
+	System.out.println("This line of code was added with GitHub edit code");
     }
 
     public static void catSound() {
