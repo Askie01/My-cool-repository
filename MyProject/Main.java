34f2e0ee5fc1b27fb0655d732c3d68b1e3e4144a
Askie01 <adam.paw01@gmail.com>
2023-07-24 17:37:07 +0200

    Weird commit

--- MyProject/Main.java
+++ MyProject/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-	System.out.println("Problem has been solved!");
+        System.out.println("Problem has been solved!");
         System.out.println("This is my first project!");
-	System.out.println("This line of code was added with GitHub edit code");
+        System.out.println("This line of code was added with GitHub edit code");
     }
 
     public static void catSound() {
         System.out.println("Meeeow!");
     }
 
-	public static void dogSound() {
+    public static void dogSound() {
         System.out.println("Woof woof.");
     }
 }
